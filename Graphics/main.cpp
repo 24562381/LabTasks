b1b770b52f0f39b500a571694357dfd707b42ce7
Sajedul Islam Sakib <246383455+sajedulisakib-001@users.noreply.github.com>
2026-06-30 22:05:55 +0600

    Add files via upload

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -3,69 +3,79 @@
 #include <OpenGL/gl.h>
 #include <GLUT/glut.h>
 
-void renderBitmapString(float x, float y, float z, void *font, char *string) {
-    char *c;
-    glRasterPos3f(x, y,z);
-    for (c=string; *c != '\0'; c++)
-    {
-        glutBitmapCharacter(font, *c);
-    }
-}
 
 void display() {
-    glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT); 
 
     glColor3f(0.0f, 1.0f, 0.0f);
     glLineWidth(2);
     glBegin(GL_LINES);
-        glVertex2f(0.0f, -1.0f);
-        glVertex2f(0.0f,  1.0f);
         glVertex2f(-1.0f, 0.0f);
         glVertex2f( 1.0f, 0.0f);
-
     glEnd();
 
-    glColor3f(1.0f, 0.2f, 0.0f);
-    glBegin(GL_TRIANGLES);
-        glVertex2f(-0.8f,  0.4f);
-        glVertex2f(-0.45f, 0.4f);
-        glVertex2f(-0.625f, 0.7f);
-    glEnd();
-    
-    glColor3f(1.0f, 0.6f, 0.0f);
+
+    /// Sweden
+    glColor3f(0.122f, 0.345f, 0.592f);
     glBegin(GL_POLYGON);
-        glVertex2f(0.40f, 0.2f);
-        glVertex2f(0.30f, 0.4f);
-        glVertex2f(0.40f, 0.6f);
-        glVertex2f(0.58f, 0.6f);
-        glVertex2f(0.68f, 0.4f);
-        glVertex2f(0.58f, 0.2f);
-
+        glVertex2f(-0.36f,0.6f);
+        glVertex2f(0.4f,0.6f);
+        glVertex2f(0.4f,0.16f);
+        glVertex2f(-0.36f,0.16f);
     glEnd();
 
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glBegin(GL_QUADS);
-        glVertex2f(-0.72f, -0.42f);
-        glVertex2f(-0.45f, -0.42f);
-        glVertex2f(-0.45f, -0.72f);
-        glVertex2f(-0.72f, -0.72f);
-    glEnd();
-
-
-    glColor3f(1.0f, 0.6f, 0.0f);
+    glColor3f(0.98f, 0.8f, 0.114f);
     glBegin(GL_POLYGON);
-        glVertex2f(0.45f, -0.72f);
-        glVertex2f(0.65f, -0.72f);
-        glVertex2f(0.54f, -0.30f);
+        glVertex2f(-0.14f,0.6f);
+        glVertex2f(-0.08f,0.6f);
+        glVertex2f(-0.08f,0.16f);
+        glVertex2f(-0.14f,0.16f);
     glEnd();
 
-     glFlush(); 
+    glColor3f(0.98f, 0.8f, 0.114f);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.36f,0.4f);
+        glVertex2f(0.4f,0.4f);
+        glVertex2f(0.4f,0.34f);
+        glVertex2f(-0.36f,0.34f);
+    glEnd();
+
+
+    ///Somalia
+    glColor3f(0.255f, 0.561f, 0.871f);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.36f,-0.16f);
+        glVertex2f(0.4f,-0.16f);
+        glVertex2f(0.4f,-0.6f);
+        glVertex2f(-0.36f,-0.6f);
+    glEnd();
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.02f,   -0.36f);
+        glVertex2f( 0.0f,    -0.28f);
+        glVertex2f( 0.04f,   -0.36f);
+        glVertex2f( 0.12f,   -0.36f);
+        glVertex2f( 0.05f,   -0.42f);
+        glVertex2f( 0.09f,   -0.52f);
+        glVertex2f( 0.01f,   -0.45f);
+        glVertex2f(-0.07f,   -0.52f);
+        glVertex2f(-0.028f,  -0.42f);
+        glVertex2f(-0.12f,   -0.36f);
+
+    glEnd();
+
+
+
+
+
+    glFlush(); 
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitWindowSize(640, 480); 
+    glutInitWindowSize(640, 640); 
     glutInitWindowPosition(80, 50);  
     glutCreateWindow("OpenGL Setup Test"); 
     glutDisplayFunc(display); 
